4d2c600c74f140e296d6815d599301ca19abd30b
Jdewi <Jackdew31@icloud.com>
2019-06-07 14:26:26 +1000

    First version, patches the c# assembly for roguelands.

---
+++ Auto-Octodiff/Auto-Octodiff/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Auto_Octodiff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Roguelands.exe";
+            this.openFileDialog1.InitialDirectory = ": %PROGRAMFILES(X86)%\\Steam\\steamapps\\common\\Roguelands\\Roguelands.exe";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Location = new System.Drawing.Point(15, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select the Roguelands.exe file. With Steam this usually located in:\r\nProgram File" +
+    "s (x86)\\Steam\\steamapps\\common\\Roguelands";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(152, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Patch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(138, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.textBox1.Location = new System.Drawing.Point(16, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(326, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBox2.Location = new System.Drawing.Point(16, 189);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(326, 60);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "1\r\n2\r\n3\r\n4\r\n5\r\n";
+            this.textBox2.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(358, 269);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Roguelands Patcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Auto-Octodiff/Auto-Octodiff/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Octodiff;
+using Octodiff.Core;
+using Octodiff.Diagnostics;
+
+namespace Auto_Octodiff
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Show the dialog that allows user to select a file, the 
+            // call will result a value from the DialogResult enum
+            // when the dialog is dismissed.
+            DialogResult result = openFileDialog1.ShowDialog();
+            // if a file is selected
+            if (result == DialogResult.OK)
+            {
+                // Set the selected file URL to the textbox
+                this.textBox1.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Apply delta file to create new file
+                var patcherPath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+                var exePath = Path.GetDirectoryName(textBox1.Text); // get the folder of Roguelands.exe
+                var assemblyDirectory = Path.Combine(exePath, @"Roguelands_Data\Managed");
+                if (!Directory.Exists(assemblyDirectory))
+                    throw new System.ArgumentException("Unable to find directory", assemblyDirectory);
+                var assemblyPath = Path.Combine(assemblyDirectory, "Assembly-CSharp.dll");
+                if (!File.Exists(assemblyPath))
+                    throw new System.ArgumentException("Unable to find file", assemblyPath);
+                var cleanPath = Path.Combine(assemblyDirectory, "Original_Assembly-CSharp.dll");
+                if(!File.Exists(cleanPath))
+                    File.Copy(assemblyPath, cleanPath, true);
+
+                var deltaApplier = new DeltaApplier { SkipHashCheck = false };
+                using (var basisStream = new FileStream(cleanPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (var deltaStream = new FileStream(Path.Combine(patcherPath, "patch"), FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (var newFileStream = new FileStream(assemblyPath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read))
+                {
+                    deltaApplier.Apply(basisStream, new BinaryDeltaReader(deltaStream, new ConsoleProgressReporter()), newFileStream);
+                }
+            }
+            catch(Exception err)
+            {
+                textBox2.Text = err.Message;
+            }
+            
+
+            //"octodiff patch vanillaFile modDelta.octodelta newFile --progress"
+        }
+    }
+}
